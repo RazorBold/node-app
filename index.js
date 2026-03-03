7c1aba897f0052f3467d781c8f9d827d211092dc
Muhammad Syaiful R <muhammad.syaiful250198@gmail.com>
2026-03-03 16:24:35 +0700

    ganti ali

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Node CI/CD is working 🚀 Syaiful ganteng");
+  res.send("Node CI/CD is working 🚀 Syaiful ganteng banget");
 });
 
 app.listen(5028, () => {
